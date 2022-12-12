98c467c378ca15dd5069f144f8418d9bf2c79f64
Francesco Giordano <giordafr@amazon.it>
2022-12-12 19:02:03 +0100

    debug

--- dist/index.js
+++ dist/index.js
@@ -10660,8 +10660,9 @@ async function run() {
     core.debug("Get github token")
     // get information on everything
     const token = core.getInput('github-token', { required: true })
-    const octokit = github.getOctokit(token)
-    const context = github.context
+    const octokit = github.getOctokit(token);
+    const context = github.context;
+    //const payload = ((context.eventName === 'push') ? context.payload.push : context.payload.pull_request);
 
     core.debug("Check body contains")
     // Check that the pull request description contains the required string
@@ -10695,7 +10696,7 @@ async function run() {
 
     core.debug("Check max files changed")
     // Check that no more than the specified number of files were changed
-    const maxFilesChanged = parseInt(core.getInput('maxFilesChanged'))
+    const maxFilesChanged = core.getInput('maxFilesChanged')
     if (typeof maxFilesChanged !== 'undefined') {
       if (maxFilesChanged && files.length > maxFilesChanged) {
         core.setFailed("The PR shouldn not change more than " + maxFilesChanged + " file(s)");
@@ -10720,7 +10721,7 @@ async function run() {
 
     core.debug("Check max lines changed")
     // Check that no more than the specified number of lines have changed
-    const maxLinesChanged = parseInt(core.getInput('maxLinesChanged'))
+    const maxLinesChanged = core.getInput('maxLinesChanged')
     if (typeof maxLinesChanged !== 'undefined') {
       if (maxLinesChanged && (additions > maxLinesChanged)) {
         core.setFailed("The PR shouldn not change more than " + maxLinesChanged + " lines(s) ");
@@ -10758,7 +10759,6 @@ async function run() {
         core.setFailed("The PR diff should not include a string matching" + diffDoesNotContainRegex);
       }
     }
-
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
